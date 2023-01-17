4213427468d1356bf660295561e2e64319e3e007
Randi <randipa@keaaerospace.com>
2023-01-18 09:52:28 +1300

    Added relative path the inactive split

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,22 @@
+local file_section = {
+  'filename',
+  file_status = true, -- Displays file status (readonly status, modified status)
+  newfile_status = false, -- Display new file status (new file means no write after created)
+  -- 0: Just the filename
+  -- 1: Relative path
+  -- 2: Absolute path
+  -- 3: Absolute path, with tilde as the home directory
+  path = 1,
+  shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+  -- for other components. (terrible name, any suggestions?)
+  symbols = {
+    modified = '[+]', -- Text to show when the file is modified.
+    readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+    unnamed = '[No Name]', -- Text to show for unnamed buffers.
+    newfile = '[New]', -- Text to show for new created file before first writting
+  }
+}
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -20,27 +39,7 @@ require('lualine').setup {
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch' },
-    --lualine_c = {'filename'},
-    lualine_c = {
-      {
-        'filename',
-        file_status = true, -- Displays file status (readonly status, modified status)
-        newfile_status = false, -- Display new file status (new file means no write after created)
-        -- 0: Just the filename
-        -- 1: Relative path
-        -- 2: Absolute path
-        -- 3: Absolute path, with tilde as the home directory
-        path = 1,
-        shorting_target = 40, -- Shortens path to leave 40 spaces in the window
-        -- for other components. (terrible name, any suggestions?)
-        symbols = {
-          modified = '[+]', -- Text to show when the file is modified.
-          readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
-          unnamed = '[No Name]', -- Text to show for unnamed buffers.
-          newfile = '[New]', -- Text to show for new created file before first writting
-        }
-      }
-    },
+    lualine_c = { file_section },
     lualine_x = { 'filetype' },
     lualine_y = { '' },
     lualine_z = { 'location' }
@@ -48,7 +47,7 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { 'filename' },
+    lualine_c = { file_section },
     lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
