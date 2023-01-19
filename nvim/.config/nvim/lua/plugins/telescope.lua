8b1298aa47902f786c3f203092ae51590ec6a06d
Randi <randipa@keaaerospace.com>
2023-01-19 16:31:49 +1300

    Added keybind to only search in the open file

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -10,5 +10,6 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fs', function() builtin.live_grep({ search_dirs = { vim.fn.expand("%:p") } }) end, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
