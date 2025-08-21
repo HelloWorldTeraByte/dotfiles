4965900e22f85f6bb2d3f03427c483f4fd49876b
hwtb <randipa99@gmail.com>
2025-08-22 00:45:28 +1200

    Using polybarFull for audio

--- configuration.nix
+++ configuration.nix
@@ -74,7 +74,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    polybar
+    polybarFull
     rofi
     feh
 
