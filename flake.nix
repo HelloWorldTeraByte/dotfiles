640bdee3d6d045349f29a1fccc6db6c690549768
hwtb <randipa99@gmail.com>
2025-08-21 19:35:18 +1200

    Moving nixos configs here

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Main flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+    in
+    {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.users.hwtb = ./home.nix;
+            }
+          ];
+        };
+      };
+    };
+}
