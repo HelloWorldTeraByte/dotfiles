4c52d2eca6d2bc9ceaa1d28f661df2dba8cdd0d7
hwtb <randipa99@gmail.com>
2025-08-22 19:11:29 +1200

    Using closed source nvidia driver :(, and added lock screen

--- configuration.nix
+++ configuration.nix
@@ -14,8 +14,48 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  #TODO: Move this to hardware specific module.
-  boot.initrd.luks.devices."luks-f290b79a-5c72-481c-86e8-2d74474e0a76".device = "/dev/disk/by-uuid/f290b79a-5c72-481c-86e8-2d74474e0a76";
+  #TODO: Move these to hardware specific module.
+  ##############################################################################
+  boot.initrd.luks.devices."luks-f290b79a-5c72-481c-86e8-2d74474e0a76".device =
+    "/dev/disk/by-uuid/f290b79a-5c72-481c-86e8-2d74474e0a76";
+
+  hardware.graphics = {
+    enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = true;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    open = true;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+  ##############################################################################
 
   networking.hostName = "hwtb-home0"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -50,7 +90,9 @@
   services.xserver = {
     enable = true;
 
-    windowManager.i3 = { enable = true; };
+    windowManager.i3 = {
+      enable = true;
+    };
 
     # Configure keymap in X11
     xkb = {
@@ -58,12 +100,16 @@
       variant = "";
     };
   };
+  programs.slock.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.hwtb = {
     isNormalUser = true;
     description = "hwtb";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
     packages = with pkgs; [ ];
     shell = pkgs.fish;
   };
@@ -74,6 +120,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    xss-lock
     polybarFull
     rofi
     feh
@@ -127,6 +174,9 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
 }
