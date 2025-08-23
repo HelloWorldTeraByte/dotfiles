77ff98f056245dffb1706d39d5be01cd80e44d71
hwtb <randipa99@gmail.com>
2025-08-23 12:20:29 +1200

    Using a common nix module

--- hosts/hwtb-home0/configuration.nix
+++ hosts/hwtb-home0/configuration.nix
@@ -1,21 +1,11 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ../../nixos-modules/common.nix
   ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  #TODO: Move these to hardware specific module.
-  ##############################################################################
   boot.initrd.luks.devices."luks-f290b79a-5c72-481c-86e8-2d74474e0a76".device =
     "/dev/disk/by-uuid/f290b79a-5c72-481c-86e8-2d74474e0a76";
 
@@ -52,140 +42,8 @@
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
-  ##############################################################################
 
-  networking.hostName = "hwtb-home0"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Pacific/Auckland";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_NZ.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_NZ.UTF-8";
-    LC_IDENTIFICATION = "en_NZ.UTF-8";
-    LC_MEASUREMENT = "en_NZ.UTF-8";
-    LC_MONETARY = "en_NZ.UTF-8";
-    LC_NAME = "en_NZ.UTF-8";
-    LC_NUMERIC = "en_NZ.UTF-8";
-    LC_PAPER = "en_NZ.UTF-8";
-    LC_TELEPHONE = "en_NZ.UTF-8";
-    LC_TIME = "en_NZ.UTF-8";
-  };
-
-  fonts.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
-
-  services.xserver = {
-    enable = true;
-
-    displayManager = {
-      startx.enable = true;
-    };
-
-    windowManager.i3 = {
-      enable = true;
-    };
-
-    # Configure keymap in X11
-    xkb = {
-      layout = "us";
-      variant = "";
-    };
-  };
-  programs.slock.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.hwtb = {
-    isNormalUser = true;
-    description = "hwtb";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
-    packages = with pkgs; [ ];
-    shell = pkgs.fish;
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    xss-lock
-    polybarFull
-    rofi
-    feh
-    picom
-
-    ripgrep
-    bat
-    eza
-    dust
-    fd
-    bottom
-    difftastic
-    arandr
-
-    vim
-    git
-    alacritty
-    zellij
-
-    gcc
-    clang
-    rustup
-    nixd
-    nixfmt-rfc-style
-    vscode
-
-    firefox
-  ];
-
-  programs.fish.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
+  networking.hostName = "hwtb-home0";
 }
